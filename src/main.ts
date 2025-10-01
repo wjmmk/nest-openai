5c82478b167282323713522e4d87145dc3912b9d
William Mosquera <wjmmk3@gmail.com>
2025-10-01 14:32:39 -0500

    feat: Pipe-vlidations added.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-//import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
   app.enableCors();
-  /* app.useGlobalPipes(new ValidationPipe()); */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      /* transform: true */
+    })
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
