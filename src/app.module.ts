4728be1fc5b74e95abb6b89368bb3658cae07e1f
William Mosquera <wjmmk3@gmail.com>
2025-10-01 12:06:56 -0500

    feat: Gpt module created.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OpenaiModule } from './openai/openai.module';
 import { OutServiceService } from './openai/out-service/out-service.service';
+import { GptModule } from './gpt/gpt.module';
 
 @Module({
-  imports: [OpenaiModule],
+  imports: [OpenaiModule, GptModule],
   controllers: [AppController],
   providers: [AppService, OutServiceService],
 })
