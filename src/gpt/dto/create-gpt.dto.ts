13e44823e56d2683264daaa617c6a30554a0c936
William Mosquera <wjmmk3@gmail.com>
2025-10-02 07:30:30 -0500

    feat: Validaciones de los endpoind implemented.

--- src/gpt/dto/create-gpt.dto.ts
+++ src/gpt/dto/create-gpt.dto.ts
@@ -1,6 +1,21 @@
+import { Type } from "class-transformer";
+import { IsNumber, IsOptional, IsString } from "class-validator";
+
 export class CreateGptDto {
-    readonly title?: string | null | undefined;
+    @IsOptional()
+    @IsString()
+    readonly title?: string;
+
+    @IsString()
     readonly prompt: string;
+
+    @IsOptional()
+    @Type(() => Number)
+    @IsNumber({}, { message: 'max_tokens must be a number' })
     readonly max_tokens?: number;
+
+    @IsOptional()
+    @Type(() => Number)
+    @IsNumber({ maxDecimalPlaces: 2 }, { message: 'temperature must be a number with at most 2 decimal places' })
     readonly temperature?: number;
 }
