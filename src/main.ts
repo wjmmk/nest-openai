d0300ab6f33e5bfad484c5f0c1645bd862d09255
William Mosquera <wjmmk3@gmail.com>
2025-10-03 15:58:02 -0500

    fix: Added versioning in the principal route.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
