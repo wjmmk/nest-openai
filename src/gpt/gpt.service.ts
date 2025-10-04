5567bcd95d369aa4f168d0a0305a3265788491f9
William Mosquera <wjmmk3@gmail.com>
2025-10-04 00:24:52 -0500

    fix: Methods refactored.

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -7,72 +7,32 @@ import { GoogleGenAI } from '@google/genai';
 
 @Injectable()
 export class GptService {
-    // Este servicio es solo para llamar casos de Uso.
-
   /*  private openai = new OpenAI({
          baseURL: 'https://openrouter.ai/api/v1',
          apiKey: process.env.OPENAI_API_KEY,
    }) */
 
-  
 
    private gemini = new GoogleGenAI({apiKey: process.env.GOOGLE_API_KEY}); 
 
-
-    async callModelAi(createGptDto: CreateGptDto): Promise<any> {
-        const { prompt, title } = createGptDto;
+   async callModelAi(createGptDto: CreateGptDto): Promise<any> {
+      const { prompt, title } = createGptDto;
         //return this.callOpenRouterAPI(createGptDto);
-        const response = await this.gemini.models.generateContentStream({
+        const response = await this.gemini.models.generateContent({
             model: "gemini-2.5-flash",
-            contents: `Eres un aprendiz de ${title}. Crea un receta ${prompt}`
+            contents: `Eres un desarrollador experto en ${title}. Crea un programa que ${prompt}`,
+            config: {
+            thinkingConfig: {
+                thinkingBudget: 0, // Disables thinking
+               },
+            }
         });
 
-        for await (const chunk of response) {
-            //console.log(chunk.text);
-            return chunk.text;
-        }
+        return response.candidates?.[0]?.content?.parts?.[0]?.text ?? undefined;  
     }
 
-async callOpenRouterAPI(createGptDto: CreateGptDto): Promise<any> {
-    const { prompt, title } = createGptDto;
-    const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${process.env.GOOGLE_API_KEY}`, // Replace with your actual API key
-            "HTTP-Referer": "<YOUR_SITE_URL>", // Optional. Site URL for rankings on openrouter.ai.
-            "X-Title": "<YOUR_SITE_NAME>", // Optional. Site title for rankings on openrouter.ai.
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            "model": "google/gemini-2.5-flash",
-            "messages": [
-                {
-                    "role": "user",
-                    "content": [
-                         { role: 'user', content: `Eres un desarrollador experto en ${title}. Crea un programa que ${prompt}` },
-                        {
-                            "type": "image_url",
-                            "image_url": {
-                                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
-                            }
-                        }
-                    ]
-                }
-            ]
-        })
-    });
-    return response.json();
- }
-
-
-   orthographyCheck(createGptDto: CreateGptDto) {
-       return {
-           code: 200,
-           message: `Corrected orthography for: ${createGptDto.title}`,   
-       }
-    }
    
-   orthographyCheckUseCase(createGptDto: CreateGptDto) {
+   async orthographyCheckUseCase(createGptDto: CreateGptDto): Promise<any> {
         return orthographyUseCase(this.gemini, {
             prompt: createGptDto.prompt,
             title: createGptDto.title ?? ''
