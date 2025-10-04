10e771f3bbc9eae25111e6e8e4ea41041d94a4f2
William Mosquera <wjmmk3@gmail.com>
2025-10-04 12:10:42 -0500

    fix: More pactices.

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -7,19 +7,37 @@ interface Options {
     title: string;
 }
 
+const MAX_RETRIES = 3;
+const RETRY_DELAY_MS = 1000; // 1 second
+
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
 export const orthographyUseCase = async (gemini: GoogleGenAI, options: Options): Promise<any> => {
     const { prompt, title } = options;
 
-    const response = await gemini.models.generateContent({
+    for (let attempt = 1; attempt < MAX_RETRIES; attempt++) {
+        const response = await gemini.models.generateContent({
         model: "gemini-2.5-flash",
         contents: `Eres un desarrollador experto en ${title}. Crea un programa que ${prompt}`,
         config: {
+            systemInstruction: "You are a cat. Your name is Neko.",
+            temperature: 0.3,
+            maxOutputTokens: 1024,
             thinkingConfig: {
                 thinkingBudget: 0, // Disables thinking
             },
-        }        
+        },
+        
     });
 
-    return response.candidates?.[0]?.content?.parts?.[0]?.text ?? undefined
+    if (response.candidates?.[0]?.content?.parts?.[0]) {
+        const content = JSON.stringify(response.candidates?.[0]?.content?.parts?.[0] ?? undefined);
+        return JSON.parse(content);
+    }
+
+    await delay(RETRY_DELAY_MS);
+}
+
+return undefined;
 }
     
