bcdccffd509c1264ce61a7f7135666c36dbb172c
William Mosquera <wjmmk3@gmail.com>
2025-10-08 16:38:38 -0500

    fix: Proscons discusser use-case done.

--- src/gpt/use-cases/orthografy-discusser-case.ts
+++ src/gpt/use-cases/orthografy-discusser-case.ts
@@ -5,20 +5,24 @@ interface ProsConsOptions {
     prompt: string;
 }
 
-export const orthographyConsDiscusser = async (openai: OpenAI, options: ProsConsOptions): Promise<any> => {
+
+ export const orthographyConsDiscusser = async (openai: OpenAI, options: ProsConsOptions): Promise<any> => {
     const { prompt } = options;
       const response = await openai.chat.completions.create({  
             model: 'gemini-2.5-flash',
             messages: [
-               /* { role: 'system', content: 'You are a helpful assistant that translates English to Spanish.' }, */
                 { role: 'user', content: prompt },
                 { 
                   role: 'system', 
-                  content: `Se te dará una pregunta y tu tarea es dar una respuesta con pros y contras, la respuesta debe de ser en formato markdown,
-                           los pros y contras deben de estar en una lista` 
+                  content: `Debes responder con mucha amabilidad y dependiendo del tono como te pregunten. Se te dará un
+                            enunciado y tu tarea es dar una respuesta con pros y contras, los pros y contras deben de estar 
+                            en una lista. Sé conciso y responde en no más de dos oraciones y responde con la información esencial,
+                            evitando detalles superfluos. Al final responde por favor usando una lista de tres puntos clave.`
                 },
             ],
-            temperature: 0.3,
+            temperature: 0.2,
+            max_tokens: 1024,
       });
-        return response.choices[0].message ?? undefined;
+    
+      return JSON.stringify(response.choices[0].message?.content ?? undefined);
  }
